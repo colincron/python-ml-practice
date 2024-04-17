dfff07d8458c5b2e3ec4f79b49d5330ec3fac04d
Colin <colint.cron@gmail.com>
2024-04-17 13:59:50 -0400

    fixed typos

--- Program.py
+++ Program.py
@@ -60,7 +60,7 @@ print('Coeffients: ', regr.coef_)
 print('Intercept: ', regr.intercept_)
 
 #plot the fit line over the data
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color="red")
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color="blue")
 plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
 plt.xlabel("Engine Size")
 plt.ylabel("Emission")
@@ -71,5 +71,5 @@ test_y = np.asanyarray(test[['CO2EMISSIONS']])
 test_y_ = regr.predict(test_x)
 
 print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
-print("Resudual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
 print("R2-score: %.2f" % r2_score(test_y, test_y_))
