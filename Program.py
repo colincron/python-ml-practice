6d1c979d2a191f6c209c8d87ce8b8efca1b29505
Colin <colint.cron@gmail.com>
2024-04-17 13:33:20 -0400

    Added MAE, MSE, and RMSE evaluations

--- Program.py
+++ Program.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
 import numpy as np
-# %matplotlib inline
+from sklearn import linear_model
+from sklearn.metrics import r2_score
 
 #read the data in
 df = pd.read_csv("FuelConsumptionCo2.csv")
@@ -37,3 +38,38 @@ plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color="blue")
 plt.xlabel("Cylinders")
 plt.ylabel("Emission")
 plt.show()
+
+#create a mask to select random rows using np.random.ranb()
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+#train data distribution
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color="blue")
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+#use scikit-learn to model data
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+#the coefficients:
+print('Coeffients: ', regr.coef_)
+print('Intercept: ', regr.intercept_)
+
+#plot the fit line over the data
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color="red")
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine Size")
+plt.ylabel("Emission")
+
+#evaluation
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Resudual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y, test_y_))
